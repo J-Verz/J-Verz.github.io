cb9541fc39e67ca857a786ff33ff40b0f4411b53
J-Verz <71394762+J-Verz@users.noreply.github.com>
2025-07-17 15:21:05 +0200

    Write some first specs for vp-parser

---
+++ spec/vp-app/vp-parser/three-digit.spec.mjs
@@ -0,0 +1,25 @@
+import parse from "../../../vp-app/vp-parser.mjs"
+
+const validVPsToTest = [
+  { "vp": "135", "expectedResult": [1, 3, 5] },
+  { "vp": "128", "expectedResult": [1, 2, 4, 6] },
+  { "vp": "162", "expectedResult": [1, 3, 4, 5, 6] },
+];
+
+const invalidVPsToTest = [
+  { "vp": "168", "expectedResult": "Getal y moet gelijk zijn aan x+1" },
+];
+
+describe("VP Parser - Three digits", () => {
+  validVPsToTest.forEach((_case) => {
+    it(`can parse ${_case.vp}`, () => {
+      expect(parse(_case.vp)).toEqual(_case.expectedResult);
+    });
+  });
+
+  invalidVPsToTest.forEach((_case) => {
+    it(`can parse ${_case.vp}`, () => {
+      expect(() => parse(_case.vp)).toThrow(_case.expectedResult);
+    });
+  });
+}); 
---
+++ spec/vp-app/vp-parser/two-digit.spec.mjs
@@ -0,0 +1,26 @@
+import parse from "../../../vp-app/vp-parser.mjs"
+
+const validVPsToTest = [
+  { "vp": "14", "expectedResult": [1, 2, 3, 4] },
+  { "vp": "12", "expectedResult": [1, 2] },
+  { "vp": "17", "expectedResult": [1, 2, 3, 4, 5, 6, 7] },
+  { "vp": "10", "expectedResult": [1, 3] },
+];
+
+const invalidVPsToTest = [
+  { "vp": "18", "expectedResult": "Getallen mogen niet groter dan 7 zijn" },
+];
+
+describe("VP Parser - Two digits", () => {
+  validVPsToTest.forEach((_case) => {
+    it(`parses '${_case.vp}' correctly`, () => {
+      expect(parse(_case.vp)).toEqual(_case.expectedResult);
+    });
+  });
+
+  invalidVPsToTest.forEach((_case) => {
+    it(`throws an error for '${_case.vp}'`, () => {
+      expect(() => parse(_case.vp)).toThrow(_case.expectedResult);
+    });
+  });
+});
