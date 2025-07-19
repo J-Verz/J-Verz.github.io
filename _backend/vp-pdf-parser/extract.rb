d775cb6eeadf0f707cb7cec8a5ace147d0607331
J-Verz <71394762+J-Verz@users.noreply.github.com>
2025-07-19 23:03:54 +0200

    Replace vp-map.json with vp-map.js

--- _backend/vp-pdf-parser/extract.rb
+++ _backend/vp-pdf-parser/extract.rb
@@ -4,7 +4,9 @@ require "json"
 require "date"
 require "open-uri"
 
-uri = URI.parse (ARGV[0])
+URL = "https://www.oev-info.ch/sites/default/files/grafapla/2020/G00d.pdf"
+
+uri = URI.parse URL 
 reader = PDF::Reader.new(uri.open)
 
 PAGES_OF_INTEREST = 7..9 # Page 1 is indexed with 0...
@@ -19,7 +21,9 @@ reader.pages.slice(PAGES_OF_INTEREST).each do |page|
   end
 end
 
-output_path = File.join(__dir__, "../../assets/vp-map.json")
+output_path = File.join(__dir__, "../../assets/vp-map.js")
 File.open(output_path, "w") do |file|
-  file.write vp_mapping.to_json
+  file.write <<~JS
+    export default  #{vp_mapping.to_json}
+  JS
 end
